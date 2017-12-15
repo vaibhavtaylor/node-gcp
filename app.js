545e8a757a772ef32ff7f98bbadda4eb81c0ba06
Vaibhav Tailor <vaibhavtaylor07@gmail.com>
2017-12-15 14:24:23 +0530

    added mysql

--- app.js
+++ app.js
@@ -10,27 +10,27 @@ app.get('/', function(req, res){
 });
 
 app.get('/mysql', function(req, res){
-    res.status(200).send("User Pager");
-    // var con = mysql.createConnection({
-    //     host: "localhost",
-    //     port: 3307,
-    //     user: "root",
-    //     password: "root"
-    //   });
+    // res.status(200).send("User Pager");
+    var con = mysql.createConnection({
+        host: "35.188.161.30",
+        port: 3307,
+        user: "root",
+        password: "root"
+      });
       
-    //   con.connect(function(err) {
-    //     if (err) {
-    //         console.log("Error Mysql");
-    //         throw err;
-    //     }
-    //     console.log("connected");
-    //     res.send("Connected");
-    //     // con.query("SELECT * FROM customers", function (err, result, fields) {
-    //     //   if (err) throw err;
-    //     //   console.log(result);
-    //     //   res.send("Mysql Page");
-    //     // });
-    //   });
+      con.connect(function(err) {
+        if (err) {
+            res.status(200).send("Mysql Error");
+            throw err;
+        }
+        console.log("connected");
+        res.send("Mysql Connected");
+        // con.query("SELECT * FROM customers", function (err, result, fields) {
+        //   if (err) throw err;
+        //   console.log(result);
+        //   res.send("Mysql Page");
+        // });
+      });
 });
 
 var server = app.listen(process.env.PORT || '8080', function() {
